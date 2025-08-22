51163acb2ca486e653bb7d1f197049a3d596b1ae
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-08-22 15:28:38 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -24,5 +24,8 @@ class Player(models.Model):
     race = models.ForeignKey(
         Race, on_delete=models.CASCADE
     )
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True)
+    guild = models.ForeignKey(
+        Guild, on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
